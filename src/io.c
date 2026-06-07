8a1e11232125c7f95b0982ec95fa73ffc3e4c55c
oMatheusValim <matheus.valim1007@gmail.com>
2026-06-07 16:33:14 -0300

    mod de correção de erro do segmentation fault

--- src/io.c
+++ src/io.c
@@ -42,7 +42,9 @@ void write_csv(int **arrs, int quant, char* filename, int max_num){
 char **read_data_input(char* file_name, int quant, int max_num, int max_size){
 
     FILE *fptr;
-    int decimal_places = (int)log10(max_num) + 1;
+    char *buffer = NULL;
+    size_t buffer_size = 0;
+    ssize_t length;
 
     int buffers_size = max_size*(decimal_places + 1) + 210;
 
@@ -51,15 +53,18 @@ char **read_data_input(char* file_name, int quant, int max_num, int max_size){
     char **lines = (char **)malloc(quant * sizeof(char *));
 
     if (lines == NULL){
-        printf("Erro ao alocar memória para o csv");
+        printf("Erro ao alocar memória para o csv\n");
         return NULL;
     }
-   
+
+    for (int j = 0; j < quant; j++){
+        lines[j] = NULL;
+    }
 
     fptr = fopen(file_name, "r");
-  
     if (fptr == NULL) {
         printf("Erro ao abrir o arquivo\n");
+        free(lines);
         return NULL;
     }
 
@@ -73,15 +78,18 @@ char **read_data_input(char* file_name, int quant, int max_num, int max_size){
         if (lines[i]==NULL){
             printf("Erro na alocação de memória\n");
         }
-        if (lines[i] != NULL) 
-            strcpy(lines[i], buffer);
 
-        else
-            printf("ERRO NA ALOCACAO DA LINHA [%d]\n", i);
-            
+        lines[i] = malloc((size_t)length + 1);
+        if (lines[i] == NULL){
+            printf("Erro na alocação de memória\n");
+            break;
+        }
+
+        strcpy(lines[i], buffer);
         i++;
     }
 
+    free(buffer);
     fclose(fptr);
 
     return lines;
